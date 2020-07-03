7aa9de97806f9b9fa3cb5a54a9e27bc1cf6c3969
Hykilpikonna <admin@moecraft.cc>
2020-07-03 17:30:43 -0400

    [-] Remove java classes

--- src/test/java/Main.java
+++
@@ -1,8 +0,0 @@
-import api.ApiServer;
-
-public class Main {
-    public static void main(String[] args) {
-        ApiServer apiServer = new ApiServer(2333);
-        apiServer.register(new ExampleNode()).start();
-    }
-}
