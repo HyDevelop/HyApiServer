a9dbebb78446f79d6997770c3a3885eaddff66f6
Hykilpikonna <admin@moecraft.cc>
2020-07-03 17:26:07 -0400

    [+] Create ApiNodeManager

---
+++ src/main/kotlin/org/hydev/ApiNodeManager.kt
@@ -0,0 +1,45 @@
+package org.hydev
+
+/**
+ * Api node manager
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-07-03 12:34
+ */
+class ApiNodeManager
+{
+    private val nodes = ArrayList<ApiNode>()
+
+    /**
+     * Register one or many nodes
+     *
+     * @param nodes API Nodes that you want to register.
+     */
+    fun register(vararg nodes: ApiNode)
+    {
+        this.nodes.addAll(nodes)
+    }
+
+    /**
+     * Get api node
+     *
+     * @param path Path of the node
+     * @return Api node or null
+     */
+    operator fun get(path: String): ApiNode?
+    {
+        val lowerPath = path.toLowerCase()
+
+        for (node in nodes)
+        {
+            if (node.path == lowerPath)
+            {
+                return node
+            }
+        }
+
+        return null
+    }
+}
