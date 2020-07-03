aa3a89363e59fbb69b81d40f20b82440506b0a5e
Hykilpikonna <admin@moecraft.cc>
2020-07-03 17:25:58 -0400

    [+] Create ApiNode abstract class

---
+++ src/main/kotlin/org/hydev/ApiNode.kt
@@ -0,0 +1,17 @@
+package org.hydev
+
+/**
+ * Each api node represents a specific location on the server.
+ * (Eg. "http://.../hello/world" has the path "/hello/world")
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-07-03 12:34
+ */
+abstract class ApiNode(path: String)
+{
+    val path = path.toLowerCase()
+
+    abstract fun process(access: ApiAccess): Any
+}
