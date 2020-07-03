7b758eed12086c0d0c7659e545d57fa6df0576ad
Hykilpikonna <admin@moecraft.cc>
2020-07-03 17:27:28 -0400

    [+] Create ApiServer

---
+++ src/main/kotlin/org/hydev/ApiServer.kt
@@ -0,0 +1,24 @@
+package org.hydev
+
+import org.eclipse.jetty.server.Server
+import java.io.IOException
+import java.lang.System.err
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletResponse.*
+
+/**
+ * TODO: Write a description for this class!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-07-03 12:31
+ */
+class ApiServer(
+    val port: Int
+)
+{
+    // Registered nodes
+    val nodes = ApiNodeManager()
+
+}
