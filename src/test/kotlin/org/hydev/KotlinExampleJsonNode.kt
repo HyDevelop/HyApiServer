54fa5e24759411d5f41fcf749806468d216adc63
Hykilpikonna <admin@moecraft.cc>
2020-07-04 14:25:54 -0400

    [+] Create example json node in Kotlin

---
+++ src/test/kotlin/org/hydev/KotlinExampleJsonNode.kt
@@ -0,0 +1,25 @@
+package org.hydev
+
+import kotlinx.serialization.Serializable
+
+/**
+ * TODO: Write a description for this class!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-07-04 11:16
+ */
+class KotlinExampleJsonNode: JsonApiNode<KotlinExampleJsonNode.Model>("/json/divide", Model::class, maxLength = 80)
+{
+    /**
+     * Model of the data passed in through request body.
+     */
+    @Serializable
+    data class Model(val dividend: Double, val divisor: Double)
+
+    /**
+     * Process json
+     */
+    override fun json(access: ApiAccess, data: Model) = data.dividend / data.divisor
+}
