0be4e8c31050eb4eb6bf99ae699250abd6731909
Hykilpikonna <admin@moecraft.cc>
2020-07-04 14:35:41 -0400

    [+] Create example json node in Java with Gson

---
+++ src/test/java/org/hydev/JavaExampleJsonNode.java
@@ -0,0 +1,68 @@
+package org.hydev;
+
+import com.google.gson.Gson;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * JsonApiNode that is compatible with Java
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-07-04 10:24
+ */
+public class JavaExampleJsonNode extends JsonApiNodeJava<JavaExampleJsonNode.Model>
+{
+    /*
+     * Create parser (You only have to do this once for every json api node.
+     * I recommend putting this into the main function, but I put it here for
+     * the clarity of this demonstration.)
+     */
+    static
+    {
+        Gson gson = new Gson();
+        JsonApiNodeJava.parser = new JavaJsonParser()
+        {
+            @Override
+            public <T> T parse(@NotNull Class<T> type, @NotNull String json)
+            {
+                return gson.fromJson(json, type);
+            }
+
+            @NotNull
+            @Override
+            public <T> String stringify(@NotNull Class<T> type, @NotNull Object data)
+            {
+                return gson.toJson(data);
+            }
+        };
+    }
+
+    /**
+     * Create node
+     */
+    public JavaExampleJsonNode()
+    {
+        // (Path, Model class, Is secret, Max length).
+        super("/json/divide", Model.class, false, 80);
+    }
+
+    /**
+     * Model of the data passed in through request body.
+     */
+    static class Model
+    {
+        double dividend;
+        double divisor;
+    }
+
+    /**
+     * Process json. In this case, this api does a simple division of the
+     * two numbers given.
+     */
+    @Override
+    public Object json(@NotNull ApiAccess access, Model data)
+    {
+        return data.dividend / data.divisor;
+    }
+}
