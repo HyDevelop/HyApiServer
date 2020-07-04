485e338a53040012df8608937f5759d73b82daa3
Hykilpikonna <admin@moecraft.cc>
2020-07-04 14:35:53 -0400

    [+] Create Java tests for json api node

--- src/test/java/org/hydev/JavaTest.java
+++ src/test/java/org/hydev/JavaTest.java
@@ -1,12 +1,19 @@
 package org.hydev;
 
-import okhttp3.*;
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Request.Builder;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.hydev.KotlinExampleJsonNode.Model;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static java.lang.Double.parseDouble;
+import static java.lang.Math.floor;
 import static okhttp3.MediaType.parse;
 import static okhttp3.RequestBody.create;
 
@@ -21,7 +28,7 @@ public class JavaTest
     static void init()
     {
         server = new ApiServer(1029);
-        server.getNodes().register(new JavaExampleNode());
+        server.getNodes().register(new JavaExampleNode(), new JavaExampleJsonNode());
         server.start();
     }
 
@@ -46,7 +53,7 @@ public class JavaTest
         Request request = new Builder().url("http://localhost:1029/c/").build();
         try (Response response = http.newCall(request).execute())
         {
-            assert response.body().string().equals("Not found.");
+            assert response.body().string().equals("{\"error\":\"Not found.\"}");
             assert response.code() == 404;
         }
     }
@@ -78,6 +85,86 @@ public class JavaTest
         try (Response response = http.newCall(request).execute())
         {
             assert response.body().string().equals("Thank you! (⺣◡⺣)\nI'm a cat");
+        }
+    }
+
+    @Test
+    void testJson() throws IOException
+    {
+        Request request = new Builder()
+            .post(create(new Gson().toJson(new Model(23, 33)), parse("application/json")))
+            .url("http://localhost:1029/json/divide").build();
+
+        try (Response response = http.newCall(request).execute())
+        {
+            assert floor(parseDouble(response.body().string()) * 100) == 69;
+        }
+    }
+
+    @Test
+    void testJsonWithoutBody() throws IOException
+    {
+        Request request = new Builder()
+            .url("http://localhost:1029/json/divide").build();
+
+        try (Response response = http.newCall(request).execute())
+        {
+            assert response.body().string().equals("{\"error\":\"Request body is empty.\"}");
+        }
+    }
+
+    @Test
+    void testJsonWithIncorrectModel() throws IOException
+    {
+        Request request = new Builder()
+            .post(create("{\"name\": \"Yukari Yakumo\", \"age\": \"At least 1200anghhkjlhjhgfsdfhjklfc\"}",
+                parse("application/json")))
+            .url("http://localhost:1029/json/divide").build();
+
+        try (Response response = http.newCall(request).execute())
+        {
+            assert response.body().string().equals("NaN");
+        }
+    }
+
+    @Test
+    void testJsonWithUnparseableModel() throws IOException
+    {
+        Request request = new Builder()
+            .post(create("The quick brown fox jumps over the lazy dog.",
+                parse("application/json")))
+            .url("http://localhost:1029/json/divide").build();
+
+        try (Response response = http.newCall(request).execute())
+        {
+            assert response.body().string().startsWith("{\"error\":\"Error during json parsing: ");
+        }
+    }
+
+    @Test
+    void testJsonWithDataTooLong() throws IOException
+    {
+        Request request = new Builder()
+            .post(create("The quick brown fox didn't jump over the lazy dog, but decided to slide under the lazy dog this time.",
+                parse("application/json")))
+            .url("http://localhost:1029/json/divide").build();
+
+        try (Response response = http.newCall(request).execute())
+        {
+            assert response.body().string().equals("{\"error\":\"Body too long. (101/80)\"}");
+        }
+    }
+
+    @Test
+    void testJsonWithInternalError() throws IOException
+    {
+        Request request = new Builder()
+            .post(create(new Gson().toJson(new Model(1337, 0)), parse("application/json")))
+            .url("http://localhost:1029/json/divide").build();
+
+        try (Response response = http.newCall(request).execute())
+        {
+            assert response.body().string().equals("Infinity");
         }
     }
 }
